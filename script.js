f0b420c1d852824b9841986ff59d6277c140d7a4
joseph <joseph.bishara99@gmail.com>
2023-10-13 16:38:23 +0300

    Initial portfolio setup- added the drawings

---
+++ script.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const gallery = document.getElementById("gallery");
+    const popup = document.getElementById("popup");
+    const largeImage = document.getElementById("largeImage");
+  
+    // Sample image data. Replace with your actual image URLs.
+    const images = ["drawing1.jpg", "drawing2.jpg", "drawing3.jpg"];
+  
+    images.forEach(imgSrc => {
+      const img = document.createElement("img");
+      img.src = imgSrc;
+      img.onclick = function() {
+        largeImage.src = imgSrc;
+        popup.style.display = "block";
+      };
+      gallery.appendChild(img);
+    });
+  
+    popup.onclick = function() {
+      popup.style.display = "none";
+    };
+  });
+  
