912e4bb1396ad4f194a24de2dbe284f1740a1b53
JosephBshara <147715654+JosephBshara@users.noreply.github.com>
2023-10-13 18:04:48 +0300

    Update script.js
    test 2 pic

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const largeImage = document.getElementById("largeImage");
   
     // Sample image data. Replace with your actual image URLs.
-    const images = ["drawing1.jpg", "drawing2.jpg", "drawing3.jpg", "drawing4.jpg","drawing5.jpg", "drawing6.jpg","drawing7.jpg","drawing8.jpg","drawing9.jpg","drawing10.jpg","drawing11.jpg"];
+    const images = [ "drawing3.jpg", "drawing4.jpg","drawing5.jpg", "drawing6.jpg","drawing7.jpg","drawing8.jpg","drawing9.jpg","drawing10.jpg","drawing11.jpg"];
   
     images.forEach(imgSrc => {
       const img = document.createElement("img");
