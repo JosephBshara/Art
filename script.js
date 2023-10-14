ec78b268486429f0220749f070fa8d778a5a3da0
joseph <joseph.bishara99@gmail.com>
2023-10-14 18:37:03 +0300

    updated the pics in categories

--- script.js
+++ script.js
@@ -1,33 +1,40 @@
 document.addEventListener("DOMContentLoaded", function() {
-  const gallery = document.getElementById("gallery");
-  const popup = document.getElementById("popup");
-  const largeImage = document.getElementById("largeImage");
-
-  // Sample image data. Replace with your actual image URLs.
-  const images = ["drawing1.png", "drawing2.png", "drawing3.png", "drawing4.png", "drawing5.jpg", "drawing6.jpg", "drawing7.jpg", "drawing8.jpg", "drawing9.jpg", "drawing10.jpg", "drawing11.jpg"];
-
-  images.forEach(imgSrc => {
-    const imgWrapper = document.createElement("div");
-
-    const img = document.createElement("img");
-    img.src = imgSrc;
-    img.onclick = function() {
-      largeImage.src = imgSrc;
-      popup.style.display = "block";
-    };
-
-    const textDiv = document.createElement("div");
-    textDiv.className = 'drawing-name';
-    textDiv.innerHTML = "Drawing Name";  // Placeholder name, you can change it later
-
-    imgWrapper.appendChild(img);
-    imgWrapper.appendChild(textDiv);
+  // Common function to populate gallery
+  function populateGallery(galleryClass, imageArray) {
+    const gallery = document.querySelector(galleryClass);
+    if (!gallery) return;  // Skip if gallery element is not on the page
     
-    gallery.appendChild(imgWrapper);
-  });
+    const popup = document.getElementById("popup");
+    const largeImage = document.getElementById("largeImage");
 
-  popup.onclick = function() {
-    popup.style.display = "none";
-  };
+    imageArray.forEach(imgSrc => {
+      const imgWrapper = document.createElement("div");
+
+      const img = document.createElement("img");
+      img.src = imgSrc;
+      img.onclick = function() {
+        largeImage.src = imgSrc;
+        popup.style.display = "block";
+      };
+
+      const textDiv = document.createElement("div");
+      textDiv.className = 'drawing-name';
+      textDiv.innerHTML = "Drawing Name";
+
+      imgWrapper.appendChild(img);
+      imgWrapper.appendChild(textDiv);
+
+      gallery.appendChild(imgWrapper);
+    });
+
+    popup.onclick = function() {
+      popup.style.display = "none";
+    };
+  }
+
+  // Populate different galleries
+  populateGallery(".figure-drawings-gallery", ["drawing1.png", "drawing9.jpg", "drawing8.jpg", "drawing7.jpg"]);
+  populateGallery(".water-color-gallery", ["drawing2.png", "drawing4.png", "drawing3.png"]);
+  populateGallery(".in-progress-gallery", ["drawing11.jpg", "drawing10.jpg"]);
+  populateGallery("#gallery", ["drawing5.jpg", "drawing6.jpg", "drawing7.jpg", "drawing8.jpg", "drawing9.jpg", "drawing10.jpg", "drawing11.jpg"]);
 });
-
