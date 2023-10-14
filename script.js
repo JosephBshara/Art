216021b723f12432b7373916e6c9c983cdd4bc80
joseph <joseph.bishara99@gmail.com>
2023-10-14 19:14:13 +0300

    changed pics

--- script.js
+++ script.js
@@ -37,4 +37,17 @@ document.addEventListener("DOMContentLoaded", function() {
   populateGallery(".water-color-gallery", ["drawing2.png", "drawing4.png", "drawing3.png"]);
   populateGallery(".in-progress-gallery", ["drawing11.jpg", "drawing10.jpg"]);
   populateGallery("#gallery", ["drawing5.jpg", "drawing6.jpg", "drawing7.jpg", "drawing8.jpg", "drawing9.jpg", "drawing10.jpg", "drawing11.jpg"]);
+
+  // Code to set the active navigation link
+  let links = document.querySelectorAll('.navbar ul li a');
+  let currentPage = window.location.pathname.split('/').pop();
+  
+  links.forEach((link) => {
+    let linkPage = link.getAttribute('href');
+    if (currentPage === linkPage) {
+      link.parentElement.classList.add('active');
+    } else {
+      link.parentElement.classList.remove('active');
+    }
+  });
 });
