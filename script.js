5d4f84008cd82e3f9526807221f98cb53faa8bbf
joseph <joseph.bishara99@gmail.com>
2023-10-15 17:59:08 +0300

    art. needs fixing

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 
 document.addEventListener("DOMContentLoaded", function() {
-  // Common function to populate gallery
+    // Initialize navbar and main content for mobile view
+    if (window.innerWidth <= 768) {
+      const navbar = document.querySelector('.navbar');
+      const mainContent = document.querySelector('.main-content');
+      navbar.style.height = "15vw";
+      mainContent.style.paddingTop = "8vw";
+    }
   // Common function to populate gallery
   function populateGallery(galleryClass, imageArray) {
     const gallery = document.querySelector(galleryClass);
@@ -26,6 +32,7 @@ document.addEventListener("DOMContentLoaded", function() {
       imgWrapper.appendChild(textDiv);
 
       gallery.appendChild(imgWrapper);
+      
     });
 
     popup.onclick = function() {
@@ -61,16 +68,31 @@ document.querySelector('.art-circle').addEventListener('click', function() {
   const menuItems = document.querySelectorAll('.navbar ul li');
   const navbar = document.querySelector('.navbar');
   const mainContent = document.querySelector('.main-content');
+  const artCircle = document.querySelector('.art-circle');
   
   if (navbar.style.height === "15vw") {
     navbar.style.setProperty('height', '6vw', 'important');  
     mainContent.style.setProperty('padding-top', '0.5vw', 'important');
+    artCircle.style.setProperty('width', '8vw', 'important');  // Change size
+    artCircle.style.setProperty('height', '8vw', 'important');  // Change size
+    artCircle.style.setProperty('left', '10vw', 'important');  // Push it up
+
+    artCircle.style.setProperty('font-size', '3.5vw', 'important');  // Change font size
+    artCircle.style.setProperty('line-height', '3vw', 'important');  // Change line height
+
     menuItems.forEach(item => {
       item.style.display = "none";
     });
   } else {
     navbar.style.setProperty('height', '15vw', 'important');  
     mainContent.style.setProperty('padding-top', '8vw', 'important');
+    artCircle.style.setProperty('width', '11.75vw', 'important');  // Restore size
+      artCircle.style.setProperty('height', '11.75vw', 'important');  // Restore size
+      artCircle.style.setProperty('font-size', '5.8754vw', 'important');  // Restore font size
+      artCircle.style.setProperty('line-height', '11.75vw', 'important');  // Restore line height
+      artCircle.style.setProperty('top', '50%', 'important');  // Reset to middle
+
+
     menuItems.forEach(item => {
       item.style.display = "block";
     });
